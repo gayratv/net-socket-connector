6df20e7807345587e9e457b3da57b5282f3dbc6e
Gayrat Vlasov <ggvlasov@gmail.com>
2023-04-08 11:23:36 +0300

    fix: LoggerLevel - enum -правильно экспортировал

--- src/net-socket/index.ts
+++ src/net-socket/index.ts
@@ -3,7 +3,8 @@ export { SocketMessagingClient } from './lib/client-socket-message.js';
 export { ServerSocket } from './lib/server-socket.js';
 export { WorkerForServer, createNewJober } from './lib/worker-for-server.js';
 // обязательный контракт для логера
-export type { ILogger, LoggerLevel } from '../logger/logger.interface.js';
+export type { ILogger } from '../logger/logger.interface.js';
+export { LoggerLevel } from '../logger/logger.interface.js';
 // необязательная реализация логера
 export { NLog, SimpleLog } from '../logger/logger.implementation.js';
 export type { TBaseResultJob, Formater, RecievedServerMessages, ErrType } from './types/net-socket-types.js';
